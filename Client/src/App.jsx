c028d6688f22d9ba5e7c74428236c77774f4448e
Vedant <vedantsudhirpatil@gmail.com>
2024-08-02 23:36:13 +0530

    React-Router added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello Sumaniac !!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>Hello Sumaniac !!</h1>} />
+        </Routes>
+      </Router>
+
     </>
   )
 }
